8bf24e0d1249ea57bcedeb42bd2f4db48737417e
debe82 <saltdev@salt-devs-MacBook-Pro.local>
2023-03-15 13:20:07 +0100

    startProject

---
+++ src/main/java/se/salt/rri/RriApplication.java
@@ -0,0 +1,13 @@
+package se.salt.rri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/salt/rri/RriApplicationTests.java
@@ -0,0 +1,13 @@
+package se.salt.rri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
