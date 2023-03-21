3cfa95cc65c64d73d8b36546e041336388cdc9d1
Skonebrant <carl-fredrik.skonebrant@appliedtechnology.se>
2023-03-21 15:21:28 +0100

    first commit, spring essentials

---
+++ src/main/java/se/salt/gardnr/GardnrApplication.java
@@ -0,0 +1,13 @@
+package se.salt.gardnr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GardnrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GardnrApplication.class, args);
+	}
+
+}
