ad1515cd4c07c402cd5ce8fa9bc2c0f3e88379c6
vanhouttestien <stien@vanhoutte.eu.com>
2023-03-25 11:21:03 +0100

    fix: naming in plant model

--- src/main/java/se/salt/gardnr/plant/Plant.java
+++ src/main/java/se/salt/gardnr/plant/Plant.java
@@ -27,59 +27,5 @@ public class Plant {
     public Plant() {
     }
 
-    public int getPlantId() {
-        return plantId;
-    }
 
-    public String getPlantName() {
-        return plantName;
     }
-
-    public void setPlantName(String plantName) {
-        this.plantName = plantName;
-    }
-
-    public String getPlantNameLatin() {
-        return plantNameLatin;
-    }
-
-    public void setPlantNameLatin(String plantNameLatin) {
-        this.plantNameLatin = plantNameLatin;
-    }
-
-    public List<String> getInstructions() {
-        return instructions;
-    }
-
-    public void setInstructions(List<String> instructions) {
-        this.instructions = instructions;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public LocalDateTime getStartDate() {
-        return startDate;
-    }
-
-    public void setStartDate(LocalDateTime startDate) {
-        this.startDate = startDate;
-    }
-
-    @Override
-    public String toString() {
-        return "Plant{" +
-                "plantId=" + plantId +
-                ", plantName='" + plantName + '\'' +
-                ", plantNameLatin='" + plantNameLatin + '\'' +
-                ", instructions=" + instructions +
-                ", description='" + description + '\'' +
-                ", startDate=" + startDate +
-                '}';
-    }
-}
