bf25d8d1a12ef843a62a7cd1d35a4cb80c298d67
debe82 <saltdev@salt-devs-MacBook-Pro.local>
2023-03-27 13:35:34 +0200

    before merging attemopt n. 44

---
+++ frontend/src/app/user/[id]/userplants/page.tsx
@@ -0,0 +1,13 @@
+import Card from '@/app/components/Card';
+import { Carousel } from '@/app/components/Carousel';
+import { PlantCard } from '@/app/components/PlantCard';
+import React from 'react'
+
+const UserPlants = () => {
+  return (
+    <div>
+    </div>
+  )
+}
+
+export default UserPlants;
