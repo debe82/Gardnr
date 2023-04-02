1f8880f937b4cbd77ab98731bd3ead9fa207a4f9
vanhouttestien <stien@vanhoutte.eu.com>
2023-04-02 14:25:57 +0200

    remerge with main

--- frontend_v12/components/SpecificPlantCard.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-
-
-const SpecificPlantCard = () => {
-  return <div>specificplantcard</div>
-}
-
-export default SpecificPlantCard
--- frontend_v12/pages/user/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const page = () => {
-
-  //<link href={}
-  return (
-    <div>page</div>
-  )
-}
