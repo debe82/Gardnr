1f0da9d2d41494b4613dbb2be87794c377d7ac32
Skonebrant <carl-fredrik.skonebrant@appliedtechnology.se>
2023-04-03 15:29:58 +0200

    stripe

--- src/main/java/se/salt/gardnr/client/StripeController.java
+++ src/main/java/se/salt/gardnr/client/StripeController.java
@@ -1,2 +1,20 @@
-package se.salt.gardnr.client;public class StripeController {
+package se.salt.gardnr.client;
+
+import com.stripe.model.Charge;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/api/payment")
+public class StripeController {
+
+    private StripeClient stripeClient;
+    @Autowired
+    StripeController(StripeClient stripeClient) {
+        this.stripeClient = stripeClient;
+    }
+    @PostMapping("/charge")
+    public Charge chargeCard(@RequestHeader(value="token") String token, @RequestHeader(value="amount") Double amount) throws Exception {
+        return this.stripeClient.chargeNewCard(token, amount);
+    }
 }
